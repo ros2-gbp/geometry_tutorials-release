aa68e0d37b19e4dea636a10426c0278e738903e8
Tully Foote <tfoote@osrfoundation.org>
2015-03-03 00:33:31 -0800

    Rebase from 'debian/indigo/turtle_tf2'

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -2,6 +2,12 @@
 Changelog for package turtle_tf2
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
+0.2.2 (2015-03-03)
+------------------
+* homogenizing install rules and script locations with older versions
+* remove old roslib invocations
+* Contributors: Tully Foote
+
 0.2.1 (2014-12-17)
 ------------------
 * fixing install rules for turtle_tf2 fixes `#15 <https://github.com/ros/geometry_tutorials/issues/15>`_
--- nodes/turtle_tf2_message_broadcaster.py
+++ nodes/turtle_tf2_message_broadcaster.py
@@ -30,40 +30,41 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
+#!/usr/bin/env python
 
-import roslib
-roslib.load_manifest('turtle_tf2')
 import rospy
 
-import math
-import tf2_ros
+import turtlesim.msg
 import geometry_msgs.msg
 import turtlesim.srv
+from geometry_msgs.msg import PointStamped, Point
+from std_msgs.msg import Header
+
+
+class PointPublisher:
+    def handle_turtle_pose(self, msg, turtlename):
+        self.pub.publish(PointStamped(Header(0, rospy.rostime.get_rostime(), "/world"), Point(msg.x, msg.y, 0)))
+
+    def __init__(self):
+        self.turtlename = "turtle3"  # rospy.get_param('~turtle')
+        self.sub = rospy.Subscriber('/%s/pose' % self.turtlename,
+                                    turtlesim.msg.Pose,
+                                    self.handle_turtle_pose,
+                                    self.turtlename)
+        self.pub = rospy.Publisher('turtle_point_stamped', PointStamped, queue_size=1)
 
 if __name__ == '__main__':
-    rospy.init_node('tf2_turtle')
-
-    tfBuffer = tf2_ros.Buffer()
-    listener = tf2_ros.TransformListener(tfBuffer)
-
+    rospy.init_node('tf2_turtle_stamped_msg_publisher')
     rospy.wait_for_service('spawn')
     spawner = rospy.ServiceProxy('spawn', turtlesim.srv.Spawn)
-    spawner(4, 2, 0, 'turtle2')
+    spawner(4, 2, 0, 'turtle3')
 
-    turtle_vel = rospy.Publisher('turtle2/cmd_vel', geometry_msgs.msg.Twist, queue_size=1)
+    pp = PointPublisher()
 
-    rate = rospy.Rate(10.0)
+    pub = rospy.Publisher("turtle3/cmd_vel", geometry_msgs.msg.Twist, queue_size=1)
     while not rospy.is_shutdown():
-        try:
-            trans = tfBuffer.lookup_transform('turtle2', 'turtle1', rospy.Time.now(), rospy.Duration(1.0))
-        except tf2_ros.Exception:
-            continue
-
-        angular = 4 * math.atan2(trans.transform.translation.y, trans.transform.translation.x)
-        linear = 0.5 * math.sqrt(trans.transform.translation.x ** 2 + trans.transform.translation.y ** 2)
         msg = geometry_msgs.msg.Twist()
-        msg.linear.x = linear
-        msg.angular.z = angular
-        turtle_vel.publish(msg)
-
-        rate.sleep()
+        msg.linear.x = 1
+        msg.angular.z = 1
+        pub.publish(msg)
+        rospy.sleep(rospy.Duration(0.1))
