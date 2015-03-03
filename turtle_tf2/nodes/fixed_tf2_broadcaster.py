d993bfde83f4bb8e4f7b7fb4c808f343609077a8
Tully Foote <tfoote@osrfoundation.org>
2015-03-03 00:33:11 -0800

    Rebase from 'upstream'

--- turtle_tf2/nodes/fixed_tf2_broadcaster.py
+++ turtle_tf2/nodes/fixed_tf2_broadcaster.py
@@ -31,55 +31,39 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-import roslib
-roslib.load_manifest('turtle_tf2')
-
 import rospy
 import tf2_ros
+import tf2_msgs.msg
 import geometry_msgs.msg
-import math
 
-if __name__ == '__ STATIC main__':
-    rospy.init_node('my_tf2_broadcaster')
-    br = tf2_ros.TransformBroadcaster()
-    t = geometry_msgs.msg.TransformStamped()
-    
-    t.header.frame_id = "turtle1"
-    t.child_frame_id = "carrot1"
-    t.transform.translation.x = 0.0
-    t.transform.translation.y = 2.0
-    t.transform.translation.z = 0.0
-    t.transform.rotation.x = 0.0
-    t.transform.rotation.y = 0.0
-    t.transform.rotation.z = 0.0
-    t.transform.rotation.w = 1.0
-    
-    rate = rospy.Rate(10.0)
-    while not rospy.is_shutdown():
-        t.header.stamp = rospy.Time.now()
-        br.sendTransform(t)
-        rate.sleep()
+
+class FixedTFBroadcaster:
+
+    def __init__(self):
+        self.pub_tf = rospy.Publisher("/tf", tf2_msgs.msg.TFMessage, queue_size=1)
+
+        while not rospy.is_shutdown():
+            # Run this loop at about 10Hz
+            rospy.sleep(0.1)
+
+            t = geometry_msgs.msg.TransformStamped()
+            t.header.frame_id = "turtle1"
+            t.header.stamp = rospy.Time.now()
+            t.child_frame_id = "carrot1"
+            t.transform.translation.x = 0.0
+            t.transform.translation.y = 2.0
+            t.transform.translation.z = 0.0
+
+            t.transform.rotation.x = 0.0
+            t.transform.rotation.y = 0.0
+            t.transform.rotation.z = 0.0
+            t.transform.rotation.w = 1.0
+
+            tfm = tf2_msgs.msg.TFMessage([t])
+            self.pub_tf.publish(tfm)
 
 if __name__ == '__main__':
     rospy.init_node('my_tf2_broadcaster')
-    br = tf2_ros.TransformBroadcaster()
-    t = geometry_msgs.msg.TransformStamped()    
-    
-    t.header.frame_id = "turtle1"
-    t.child_frame_id = "carrot1"    
-    
-    rate = rospy.Rate(10.0)
-    while not rospy.is_shutdown():
-        x = rospy.Time.now().to_sec() * math.pi
-        
-        t.header.stamp = rospy.Time.now()
-        t.transform.translation.x = 10 * math.sin(x)
-        t.transform.translation.y = 10 * math.cos(x)
-        t.transform.translation.z = 0.0
-        t.transform.rotation.x = 0.0
-        t.transform.rotation.y = 0.0
-        t.transform.rotation.z = 0.0
-        t.transform.rotation.w = 1.0
-        
-        br.sendTransform(t)
-        rate.sleep()
+    tfb = FixedTFBroadcaster()
+
+    rospy.spin()
